d8109568db1ec80836cc5d0a57391bd8af0f0b4f
Garry <54540935+Garry04@users.noreply.github.com>
2024-01-17 04:30:19 +0100

    Create monitors.sh

---
+++ monitors.sh
@@ -0,0 +1,3 @@
+xrandr --output HDMI-A-0 --mode 1920x1080 --rate 60 --right-of DisplayPort-1
+xrandr --output DisplayPort-1 --mode 3440x1440 --rate 100 --primary
+xrandr --output DisplayPort-2 --mode 1920x1080 --rate 75 --left-of DisplayPort-1
